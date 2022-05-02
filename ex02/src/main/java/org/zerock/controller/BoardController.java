757f13ef67c8a98d85d6abe63ef6e915e286bcbc
GoRyeoJin <sooon9254@naver.com>
2022-05-02 16:43:31 +0900

    비즈니스 계층 구현, 프레젠테이션 계층 CRUD 구현

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,78 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		
+		log.info("list");
+		model.addAttribute("list", service.getList());
+		
+	}
+	
+	// 작성
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register : " + board);
+		
+		service.register(board);
+		
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+		// redirect: => 스프링 MVC가 내부적으로 response.sendRedirect()를 처리해줌.
+	}
+	
+	// 조회
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	// 수정
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("modify : " + board);
+		
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	// 삭제는 반드시 POST 방식으로만 처리
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		
+		log.info("remove .... " + bno);
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
+
+}
